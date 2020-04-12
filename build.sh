1dbf3042595354d4ad5e9ab0c0f6920759ac6a79
Anirudh Oppiliappan <x@icyphox.sh>
2020-04-12 11:13:40 +0530

    emergency commit system is broken

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
 gb="$1"
+
+[[ "$gb" == "" ]] && {
+    printf 'requires an argument\n'
+    exit 0
+}
+
 printf 'assembling...\n' 
 rgbasm "$gb".asm -o "$gb".o
 printf 'linking...\n'
 rgblink "$gb".o -o "$gb".gb
 printf 'fixing...\n'
 rgbfix -v -p0 "$gb".gb
+
+printf 'emulate? y/n\n'
+read -rsn1 emu
+
+[[ "$emu" == "y" ]] && {
+    ~/downloads/sameboy/SameBoy-0.12.3/build/bin/SDL/sameboy \
+    "$gb".gb
+}
